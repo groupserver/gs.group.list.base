9422e827148e4ed332fe2b39609a14cfffc4ac3a
Michael JasonSmith <mpj17@onlinegroups.net>
2015-01-15 16:28:13 -0800

    Testing the EmailMessage.compressed_subject property

--- gs/group/list/base/tests/emailmessage.py
+++ gs/group/list/base/tests/emailmessage.py
@@ -26,7 +26,8 @@ Subject: Violence
 Tonight on Ethyl the Frog we look at violence.\n'''
 
     def setUp(self):
-        self.message = EmailMessage(self.m)
+        self.message = EmailMessage(self.m, list_title='Ethyl the Frog',
+                                    group_id='ethyl')
 
     def test_check_encoding_odd(self):
         r = self.message.check_encoding('wierd')
@@ -68,18 +69,6 @@ Tonight on Ethyl the Frog we look at violence.\n'''
         r = self.message.encoding
         self.assertEqual('utf-8', r)
 
-    def test_normalise_subject_empty(self):
-        r = self.message.normalise_subject('')
-        self.assertEqual('', r)
-
-    def test_normalise_lower(self):
-        r = self.message.normalise_subject('Violence')
-        self.assertEqual('violence', r)
-
-    def test_normalise_lower_whitespace(self):
-        r = self.message.normalise_subject('Ethyl the Frog')
-        self.assertEqual('ethylthefrog', r)
-
     def test_strip_subject(self):
         r = self.message.strip_subject('[Ethyl the Frog] Violence',
                                        'Ethyl the Frog')
@@ -99,6 +88,30 @@ Tonight on Ethyl the Frog we look at violence.\n'''
         self.assertEqual('Violence', r)
 
     def test_subject_stripped(self):
-        self.message.message['Subject'] = '[Ethyl the Frog] Violence'
+        self.message.message.replace_header(
+            'Subject', '[Ethyl the Frog] The Violence of British Gangland')
         r = self.message.subject
-        self.assertEqual('Violence', r)
+        self.assertEqual('The Violence of British Gangland', r)
+
+    def test_normalise_subject_empty(self):
+        r = self.message.normalise_subject('')
+        self.assertEqual('', r)
+
+    def test_normalise_lower(self):
+        r = self.message.normalise_subject('Violence')
+        self.assertEqual('violence', r)
+
+    def test_normalise_lower_whitespace(self):
+        r = self.message.normalise_subject('The Violence of British '
+                                           'Gangland')
+        self.assertEqual('theviolenceofbritishgangland', r)
+
+    def test_compressed_subject(self):
+        r = self.message.compressed_subject
+        self.assertEqual('violence', r)
+
+    def test_compressed_subject_whitespace(self):
+        self.message.message.replace_header(
+            'Subject', '[Ethyl the Frog] The Violence of British Gangland')
+        r = self.message.compressed_subject
+        self.assertEqual('theviolenceofbritishgangland', r)
